8197b5b930fdf571ace9f833f4f8fe7e9439809b
Renan <renansteiger@live.com>
2023-09-06 09:16:01 -0300

    feat: Add dependency

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import App from './App.vue'
-import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import {fas} from "@fortawesome/free-solid-svg-icons";
 
-const app = createApp(App)
+library.add(fas);
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.component('font-awesome-icon', FontAwesomeIcon).mount('#app');
